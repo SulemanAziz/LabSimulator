627f982c88ad1c5e1531f6ca7ae9cdcd8bdfc14a
SulemanAziz <sulemanaz548@gmail.com>
2025-04-24 09:06:49 -0400

    Add files via upload

---
+++ LoginRedirect.js
@@ -0,0 +1,24 @@
+async function Validate_User(event, user,pass) {
+
+  event.preventDefault();
+
+  const response= await fetch('Server-Setup/Database/Login.db'); //SQLie Database is connected here
+  const arrayBuffer = await response.arrayBuffer(); //Converted to binary array so it can be read on the browser by sql.js
+
+  const SQL = await initSqlJs();
+
+  const db= new SQL.Database(new Uint8Array(arrayBuffer));
+
+  const query = `Select * from Users where id=? and password=?`;
+  const stmt = db.prepare(query);
+
+  stmt.bind([user, pass]);
+
+  if (stmt.step()) {
+    console.log("User found");
+    window.location.href = "Webpages/Homepage.html";
+  } else {
+    alert("Invalid username or password");
+  }
+
+}
