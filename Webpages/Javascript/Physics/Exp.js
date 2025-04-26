55806c07677b8d7b3ece398363055a5af8c731e0
SulemanAziz <sulemanaz548@gmail.com>
2025-04-26 12:33:24 +0500

    .

--- Webpages/Javascript/Physics/Exp.js
+++ Webpages/Javascript/Physics/Exp.js
@@ -1,24 +1,28 @@
 function Period(g, d, l, v, m) //v stands for viscosity
 {
-  B = (6 * Math.Pi * v * (d / 2)); //Coeffecient
-  Period = (B * B) * (l / 4) * (m * m) * g;
-  return Period + " seconds";
+  g=parseFloat(g);
+  d=parseFloat(d);
+  l=parseFloat(l);
+  v=parseFloat(v);
+  m=parseFloat(m);
+
+  const B = (6 * Math.Pi * v * (d / 2)); //Coeffecient
+
+  const Period = (B * B) * (l / 4) * (m * m) * g;
+  document.getElementById("Period").innerText = Period + " seconds";
 }
 
 function ParabolicMotion() {
-
+  
 }
 
 function Hooks_Law(m, g, x) {
 
-  let mass = parseInt(m);
-  let gravity = parseInt(g);
-  let x0 = parseInt(x);
-  console.log(mass);
+  let mass = parseFloat(m);
+  let gravity = parseFloat(g);
+  let x0 = parseFloat(x);
   let k = (mass * gravity) / x0; //Hook's Law
 
-  console.log(typeof(k));
-  console.log(k);
   const random = Math.random();
 
   if (random % 2 == 0) {
