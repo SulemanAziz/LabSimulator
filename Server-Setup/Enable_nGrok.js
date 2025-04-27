24185f414f0fe69dbc01acd21e16ece81d015e72
SulemanAziz <sulemanaz548@gmail.com>
2025-04-28 03:54:37 +0500

    Fixes

--- Server-Setup/Enable_nGrok.js
+++ Server-Setup/Enable_nGrok.js
@@ -1,2 +1,3 @@
-ngrok.connect({ addr: 8080, authtoken_from_env: true })
+const ngrok = require('@ngrok/ngrok');
+ngrok.connect({ addr: 8080, authtoken_from_env: true }) //Make sure the port number set here matches the one in your EnableServer.cjs File.
 	.then(listener => console.log(`Ingress established at: ${listener.url()}`));
